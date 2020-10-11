7751b3f70c8ead607fafd581151b29ed7993deda
heavenOSK <ryunosukeheaven.ba@gmail.com>
2020-10-11 23:54:53 +0900

    improve example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:aligned_bezier_painter/aligned_bezier_painter.dart';
 import 'package:example/guid_painter.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
+final _snapThreshold = 0.05;
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -13,6 +15,7 @@ class MyApp extends StatelessWidget {
   }
 }
 
+// TODO(HeavenOSK): Refactor
 class Home extends StatefulWidget {
   const Home();
 
@@ -35,6 +38,23 @@ class _HomeState extends State<Home> {
 
   bool _guideOn = true;
 
+  String get generated => '''
+CustomPaint(
+  painter: AlignedBezierPainter(
+    strokeColor: Colors.red,
+    strokeWidth: 3,
+    startAlignment: 
+      Alignment(${_startAlignment.x},${_startAlignment.y}),
+    firstControlAlignment:  
+      Alignment(${_firstControl.x},${_firstControl.y}),
+    secondControlAlignment: 
+      Alignment(${_secondControl.x},${_secondControl.y}),
+    endAlignment: 
+      Alignment(${_endAlignment.x},${_endAlignment.y}),
+  ),
+),
+  ''';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -46,20 +66,16 @@ class _HomeState extends State<Home> {
             child: FractionallySizedBox(
               heightFactor: 0.95,
               widthFactor: 0.95,
-              child: LayoutBuilder(
-                builder: (_context, constraints) {
-                  final viewPortSize = Size(
-                    constraints.maxWidth,
-                    constraints.maxHeight,
-                  );
+              child: AspectRatio(
+                aspectRatio: 1 / 1,
+                child: LayoutBuilder(
+                  builder: (_context, constraints) {
+                    final viewPortSize = Size(
+                      constraints.maxWidth,
+                      constraints.maxHeight,
+                    );
 
-                  return Container(
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                        color: Colors.blue,
-                      ),
-                    ),
-                    child: GridPaper(
+                    return GridPaper(
                       child: Stack(
                         overflow: Overflow.visible,
                         children: [
@@ -87,7 +103,7 @@ class _HomeState extends State<Home> {
                               ),
                             ),
                           if (_guideOn)
-                            PositionedGuideCircle(
+                            _GuideCircle(
                               top: _startAlignment.alongSize(viewPortSize).dy -
                                   _GuideCircle.radius / 2,
                               left: _startAlignment.alongSize(viewPortSize).dx -
@@ -104,9 +120,25 @@ class _HomeState extends State<Home> {
                                   );
                                 });
                               },
+                              onUp: () {
+                                final x = _startAlignment.x;
+                                final y = _startAlignment.y;
+                                final shouldSnapX =
+                                    (1 - x.abs()) <= _snapThreshold;
+                                final shouldSnapY =
+                                    (1 - y.abs()) <= _snapThreshold;
+                                if (shouldSnapX || shouldSnapY) {
+                                  setState(() {
+                                    _startAlignment = Alignment(
+                                      shouldSnapX ? x.roundToDouble() : x,
+                                      shouldSnapY ? y.roundToDouble() : y,
+                                    );
+                                  });
+                                }
+                              },
                             ),
                           if (_guideOn)
-                            PositionedGuideCircle(
+                            _GuideCircle(
                               left: _firstControl.alongSize(viewPortSize).dx -
                                   _GuideCircle.radius / 2,
                               top: _firstControl.alongSize(viewPortSize).dy -
@@ -123,56 +155,125 @@ class _HomeState extends State<Home> {
                                   );
                                 });
                               },
+                              onUp: () {
+                                final x = _firstControl.x;
+                                final y = _firstControl.y;
+                                final shouldSnapX =
+                                    (1 - x.abs()) <= _snapThreshold;
+                                final shouldSnapY =
+                                    (1 - y.abs()) <= _snapThreshold;
+                                if (shouldSnapX || shouldSnapY) {
+                                  setState(() {
+                                    _firstControl = Alignment(
+                                      shouldSnapX ? x.roundToDouble() : x,
+                                      shouldSnapY ? y.roundToDouble() : y,
+                                    );
+                                  });
+                                }
+                              },
                             ),
                           if (_guideOn)
-                            PositionedGuideCircle(
+                            _GuideCircle(
                               left: _secondControl.alongSize(viewPortSize).dx -
                                   _GuideCircle.radius / 2,
                               top: _secondControl.alongSize(viewPortSize).dy -
                                   _GuideCircle.radius / 2,
                               onMove: (Offset local) {
-                                final xp = local.dx / viewPortSize.width;
-                                final yp = local.dy / viewPortSize.height;
-                                final newX = _secondControl.x + xp * 2;
-                                final newY = _secondControl.y + yp * 2;
                                 setState(
                                   () {
-                                    _secondControl = Alignment(
-                                      newX,
-                                      newY,
+                                    _secondControl = newAlignment(
+                                      viewPortSize,
+                                      _secondControl,
+                                      local,
                                     );
                                   },
                                 );
                               },
+                              onUp: () {
+                                final x = _secondControl.x;
+                                final y = _secondControl.y;
+                                final shouldSnapX =
+                                    (1 - x.abs()) <= _snapThreshold;
+                                final shouldSnapY =
+                                    (1 - y.abs()) <= _snapThreshold;
+                                if (shouldSnapX || shouldSnapY) {
+                                  setState(() {
+                                    _secondControl = Alignment(
+                                      shouldSnapX ? x.roundToDouble() : x,
+                                      shouldSnapY ? y.roundToDouble() : y,
+                                    );
+                                  });
+                                }
+                              },
                             ),
-                          PositionedGuideCircle(
-                            left: _endAlignment.alongSize(viewPortSize).dx -
-                                _GuideCircle.radius / 2,
-                            top: _endAlignment.alongSize(viewPortSize).dy -
-                                _GuideCircle.radius / 2,
-                            onMove: (Offset local) {
-                              final xp = local.dx / viewPortSize.width;
-                              final yp = local.dy / viewPortSize.height;
-                              final newX = _endAlignment.x + xp * 2;
-                              final newY = _endAlignment.y + yp * 2;
-                              setState(() {
-                                _endAlignment = Alignment(
-                                  newX,
-                                  newY,
-                                );
-                              });
-                            },
-                          ),
+                          if (_guideOn)
+                            _GuideCircle(
+                              left: _endAlignment.alongSize(viewPortSize).dx -
+                                  _GuideCircle.radius / 2,
+                              top: _endAlignment.alongSize(viewPortSize).dy -
+                                  _GuideCircle.radius / 2,
+                              onMove: (Offset local) {
+                                setState(() {
+                                  _endAlignment = newAlignment(
+                                    viewPortSize,
+                                    _endAlignment,
+                                    local,
+                                  );
+                                });
+                              },
+                              onUp: () {
+                                final x = _endAlignment.x;
+                                final y = _endAlignment.y;
+                                final shouldSnapX =
+                                    (1 - x.abs()) <= _snapThreshold;
+                                final shouldSnapY =
+                                    (1 - y.abs()) <= _snapThreshold;
+                                if (shouldSnapX || shouldSnapY) {
+                                  setState(() {
+                                    _endAlignment = Alignment(
+                                      shouldSnapX ? x.roundToDouble() : x,
+                                      shouldSnapY ? y.roundToDouble() : y,
+                                    );
+                                  });
+                                }
+                              },
+                            ),
                         ],
                       ),
-                    ),
-                  );
-                },
+                    );
+                  },
+                ),
               ),
             ),
           ),
           Expanded(
-            child: Text(''),
+            child: Container(
+              padding: EdgeInsets.all(16),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Sample code',
+                        style: Theme.of(context).textTheme.headline6,
+                      ),
+                      OutlineButton(
+                        onPressed: () {
+                          Clipboard.setData(ClipboardData(text: generated));
+                        },
+                        child: Text('COPY ALL'),
+                      ),
+                    ],
+                  ),
+                  FittedBox(
+                    child: SelectableText(
+                      generated,
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ),
         ],
       ),
@@ -186,19 +287,39 @@ class _HomeState extends State<Home> {
       ),
     );
   }
+
+  Alignment newAlignment(
+    Size viewPortSize,
+    Alignment old,
+    Offset local,
+  ) {
+    final xp = local.dx / viewPortSize.width;
+    final yp = local.dy / viewPortSize.height;
+    final newX = old.x + xp * 2;
+    final newY = old.y + yp * 2;
+
+    return Alignment(
+      newX,
+      newY,
+    );
+  }
 }
 
-class PositionedGuideCircle extends StatelessWidget {
-  const PositionedGuideCircle({
-    Key key,
+class _GuideCircle extends StatelessWidget {
+  const _GuideCircle({
     @required this.left,
     @required this.top,
     @required this.onMove,
+    @required this.onUp,
+    Key key,
   }) : super(key: key);
 
   final double left;
   final double top;
-  final Null Function(Offset localDelta) onMove;
+  final ValueChanged<Offset> onMove;
+  final VoidCallback onUp;
+
+  static double get radius => 16.0 + 12 * 2;
 
   @override
   Widget build(BuildContext context) {
@@ -206,35 +327,23 @@ class PositionedGuideCircle extends StatelessWidget {
       left: left,
       top: top,
       child: Listener(
+        onPointerUp: (d) {
+          onUp?.call();
+        },
         onPointerMove: (PointerMoveEvent detail) {
           onMove?.call(detail.localDelta);
         },
         behavior: HitTestBehavior.opaque,
-        child: _GuideCircle(),
-      ),
-    );
-  }
-}
-
-class _GuideCircle extends StatelessWidget {
-  const _GuideCircle({
-    this.dragging = false,
-  });
-
-  final bool dragging;
-
-  static double get radius => 16.0 + 12 * 2;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 16.0,
-      width: 16.0,
-      margin: EdgeInsets.all(12),
-      decoration: BoxDecoration(
-        color: Colors.transparent,
-        border: Border.all(color: dragging ? Colors.red : Colors.black),
-        borderRadius: BorderRadius.circular(16.0),
+        child: Container(
+          height: 16.0,
+          width: 16.0,
+          margin: EdgeInsets.all(12),
+          decoration: BoxDecoration(
+            color: Colors.transparent,
+            border: Border.all(),
+            borderRadius: BorderRadius.circular(16.0),
+          ),
+        ),
       ),
     );
   }
